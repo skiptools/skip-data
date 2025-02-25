3a867801bf433d972044403e5931c60fe0169e3d
Marc Prud'hommeaux <marc@skip.tools>
2025-02-25 15:50:13 -0500

    Update copyright and license headers

--- Package.swift
+++ Package.swift
@@ -6,11 +6,11 @@ let package = Package(
     defaultLocalization: "en",
     platforms: [.iOS(.v16), .macOS(.v13), .tvOS(.v16), .watchOS(.v9), .macCatalyst(.v16)],
     products: [
-    .library(name: "SkipData", targets: ["SkipData"]),
+        .library(name: "SkipData", targets: ["SkipData"]),
     ],
     dependencies: [
-        .package(url: "https://source.skip.tools/skip.git", from: "0.8.19"),
-        .package(url: "https://source.skip.tools/skip-model.git", from: "0.5.4"),
+        .package(url: "https://source.skip.tools/skip.git", from: "1.0.0"),
+        .package(url: "https://source.skip.tools/skip-model.git", from: "1.0.0"),
         .package(url: "https://source.skip.tools/skip-sql.git", from: "0.6.0"),
     ],
     targets: [
